1812a43c59203f3e3fd4b9a418b870ddd8ee3e9f
Pedro J <pjjimenezochoa@gmail.com>
2024-05-26 07:42:55 -0500

    Fix datasource paths

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -3,12 +3,20 @@ import { DataSource } from 'typeorm'
 
 dotenv.config()
 
+let entitiesPath = 'src/**/*.entity.ts'
+let migrationsPath = 'src/database/migrations/*.ts'
+
+if (process.env.NODE_ENV === 'production') {
+  entitiesPath = 'dist/src/**/*.entity.js'
+  migrationsPath = 'dist/src/database/migrations/*.js'
+}
+
 export const AppDataSource = new DataSource({
   type: 'mysql',
   url: process.env.DATABASE_URL,
   synchronize: false,
   logging: true,
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/database/migrations/*.ts'],
+  entities: [entitiesPath],
+  migrations: [migrationsPath],
   migrationsTableName: 'migrations',
 })
