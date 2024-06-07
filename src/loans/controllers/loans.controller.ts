794ddc62d7d1df7b7de14ee844a042ebbaee7799
Pedro J <pjjimenezochoa@gmail.com>
2024-06-06 23:19:59 -0500

    Replace loans filter dto to filter paginator dto

--- src/loans/controllers/loans.controller.ts
+++ src/loans/controllers/loans.controller.ts
@@ -2,8 +2,8 @@ import { Body, Controller, Get, Param, Post, Query, UseGuards } from '@nestjs/co
 import { CreateLoanDto } from 'src/loans/dtos/loans.dto'
 import { LoansService } from 'src/loans/services/loans.service'
 import { InterestsService } from 'src/loans/services/interests.service'
-import { FilterLoansDto } from '../dtos/filter-loans.dto'
 import { CreateInstallmentDto } from '../dtos/create-installment.dto'
+import { FilterPaginatorDto } from 'src/lib/filter-paginator/dtos/filter-paginator.dto'
 
 @Controller('loans')
 export class LoansController {
@@ -18,7 +18,7 @@ export class LoansController {
   }
 
   @Get()
-  findAll(@Query() params: FilterLoansDto) {
+  findAll(@Query() params: FilterPaginatorDto) {
     return this.loansService.findAll(params)
   }
 
