5fe0794c466366c8752cd63c2df936b2fc5507ea
Pedro J <pjjimenezochoa@gmail.com>
2024-10-18 03:14:11 -0500

    Add missing dependency to InstallmentService

--- src/loans/modules/installments/installments.module.ts
+++ src/loans/modules/installments/installments.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Installment } from 'src/loans/entities/installment.entity'
 import { LoansManagementModule } from '../loans-management/loans-management.module'
 import { InstallmentRepository } from 'src/loans/repositories/installment.repository'
+import { InterestsModule } from '../interests/interests.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Installment]), LoansManagementModule],
+  imports: [TypeOrmModule.forFeature([Installment]), LoansManagementModule, InterestsModule],
   providers: [InstallmentsService, InstallmentRepository],
   exports: [InstallmentsService],
 })
